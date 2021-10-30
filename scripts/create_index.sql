fe9dd9a5cae3a562315791421a2043979f44e0f2
Yuu <24975893+YuuYouYoo@users.noreply.github.com>
2021-10-30 16:25:23 +0800

    updated order_by_customer to account for queries used in OrderStatusTransaction

--- scripts/create_index.sql
+++ scripts/create_index.sql
@@ -5,7 +5,7 @@ DROP INDEX IF EXISTS wholesale.orders@undelivered_orders CASCADE;
 CREATE INDEX undelivered_orders ON wholesale.orders (O_CARRIER_ID) WHERE O_CARRIER_ID = -1;
 
 DROP INDEX IF EXISTS wholesale.orders@order_by_customer CASCADE;
-CREATE INDEX order_by_customer ON wholesale.orders (O_W_ID, O_D_ID, O_C_ID);
+CREATE INDEX order_by_customer ON wholesale.orders (O_C_ID) STORING (O_ENTRY_D, O_CARRIER_ID);
 
 DROP INDEX IF EXISTS wholesale.customer@customer_balance CASCADE;
 CREATE INDEX customer_balance ON wholesale.customer (C_BALANCE DESC) STORING (C_FIRST, C_MIDDLE, C_LAST);
